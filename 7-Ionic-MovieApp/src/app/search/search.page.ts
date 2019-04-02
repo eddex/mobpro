e4ed1645b0d08f162f648f7d07a1a9e420a3413f
eddex <eddex@outlook.com>
2019-04-02 19:23:45 +0200

    implemented movie search request and alert message on fail

--- 7-Ionic-MovieApp/src/app/search/search.page.ts
+++ 7-Ionic-MovieApp/src/app/search/search.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Movie } from '../interfaces/Movie';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPage implements OnInit {
 
-  constructor() { }
+  inputMovieTitle = '';
+
+  constructor(public httpClient: HttpClient, public alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async presentAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  searchForMovie() {
+    console.log('searchForMovie');
+    console.log(this.inputMovieTitle);
+
+    const movie = this.httpClient.get('http://www.omdbapi.com/?apikey=e047e24f&t=' + this.inputMovieTitle);
+    movie.subscribe(data => {
+      let movieData: Movie = <Movie>data;
+      console.log(movieData);
+
+      if (movieData.Response === 'False') {
+        this.presentAlert(movieData.Error);
+      } else {
+        // TODO: open details view
+      }
+    });
   }
 
 }
